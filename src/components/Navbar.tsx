c56dbf5b2c067a2d67018073af48269db45abbea
Matthew Stringer <mdsflyboy@gmail.com>
2021-06-07 19:22:00 -0700

    adding challenge assets and working on navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/layout";
+import React from "react";
+
+export type NavbarProps = {};
+
+const Navbar: React.FC<NavbarProps> = ({}) => {
+  return (
+    <Box pt={50} pl={100}>
+      Text
+    </Box>
+  );
+};
+export default Navbar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
+import { Flex } from "@chakra-ui/layout";
+import Navbar from "../components/Navbar";
 
-import { Flex } from '@chakra-ui/layout';
-import Navbar from '../components/Navbar';
-
-const Index:React.FC = ({}) => {
+const Index: React.FC = ({}) => {
   return (
     <Flex height="100vh">
-      <Navbar/>
+      <Navbar />
     </Flex>
-  )
-} 
+  );
+};
 
 export default Index;
