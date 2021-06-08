b0e3d8c0ffb7cc7c3dfd61ce7561be6f155043ee
mdsflyboy <mdsflyboy@gmail.com>
2021-06-07 18:16:33 -0700

    initial commit

--- src/theme.tsx
+++ src/theme.tsx
@@ -12,7 +12,13 @@ const breakpoints = createBreakpoints({
 
 const theme = extendTheme({
   colors: {
-    black: '#16161D',
+    lightRed: 'hsl(356, 100%, 66%)',
+    veryLightRed: 'hsl(355, 100%, 74%)',
+    veryDarkBlue: 'hsl(208, 49%, 24%)',
+    white: 'hsl(0, 0%, 100%)',
+    grayishBlue: 'hsl(208, 49%, 24%)',
+    veryDarkGrayishBlue: 'hsl(208, 49%,24%)',
+    veryDarkBlackBlue: 'hsl(240, 10%, 16%)'
   },
   fonts,
   breakpoints,
